360de340327f5a8633d1cd861e5ea32f0839c695
Janam-Babi <146522275+janambabi@users.noreply.github.com>
2026-06-16 02:16:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,4 @@ app.use('/api/students', studentRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get('/', (req, res) => {
-  res.send('Attenda Backend is Running 🚀');
-});
+
